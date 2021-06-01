bfed04d3424cbd0548b25bd51690b3dbbfe9bd65
Alice9th <monicazapata.delvalle@gmail.com>
2021-06-01 12:27:18 -0500

    JAVA Data types

---
+++ EXCERCISES/data_types.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.printIn("Mi primer programa");
+    }
+}
+
+// Data types in JAVA
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // escribe tu codigo aqui
+        int numeroEntero = 2;
+        double numeroConComa = 2.0;
+        String nombre = "Nombre";
+
+
+
+    }
+}
